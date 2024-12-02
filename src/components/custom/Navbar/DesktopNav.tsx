858e1aaafc940d07df5a9afc755bbaeadbe0b05d
shreyansh awadhiya <shreyanshawadhiya@gmail.com>
2024-12-02 18:19:36 +0530

    ui update

--- src/components/custom/Navbar/DesktopNav.tsx
+++ src/components/custom/Navbar/DesktopNav.tsx
@@ -7,7 +7,7 @@ import Credit from '@/components/custom/Credit'
 
 export default function DesktopNav() {
     return (
-        <div className='absolute inset-0 w-full h-24 max-w-7xl container flex justify-between p-4'>
+        <div className='absolute inset-0 w-full h-24 max-w-7xl container flex justify-between p-4 z-50'>
             <Link href="/" passHref legacyBehavior>
                 <Image src='/Logo.svg' width={200} height={200} alt='Project Logo' decoding="async" priority />
             </Link>

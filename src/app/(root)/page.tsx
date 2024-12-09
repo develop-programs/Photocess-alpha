b8c9211a94bb6eadc27889198c2518030b200b02
shreyansh awadhiya <shreyanshawadhiya@gmail.com>
2024-12-09 19:24:22 +0530

    ui updates

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -11,7 +12,7 @@ export default function page() {
         className="h-[calc(100vh-6rem)]"
         style={{
           background:
-            "radial-gradient(155.35% 155.35% at 50% -17.73%, #000 0%, #191919 43.63%, #343434 68.62%, #666 100%)",
+            "radial-gradient(155.35% 155.35% at 50% -17.73%, #000 0%, #191919 43.63%, #343434 68.62%, #666 100%)"
         }}
       >
         <div className="h-full mt-24 max-w-6xl mx-auto flex justify-center">
@@ -27,7 +28,7 @@ export default function page() {
       <div
         className="relative w-full h-44"
         style={{
-          background: "linear-gradient(180deg, #565656 -48.47%, #000 87.79%)",
+          background: "linear-gradient(180deg, #565656 -48.47%, #000 87.79%)"
         }}
       >
         <div className="absolute -mt-52 w-full mx-auto flex items-center justify-center">
@@ -40,9 +41,101 @@ export default function page() {
         </div>
       </div>
 
-      <div className="h-screen max-w-6xl mx-auto flex flex-col justify-end items-center py-12">
+      <div className="max-w-6xl mx-auto flex flex-col justify-end items-center py-12">
+        {/* Service section */}
+        <div className="container py-16 mt-80">
+          <div className="grid gap-8 py-16">
+            {[
+              {
+                title: "Background Remover",
+                description:
+                  "Photocess background remover is a powerful tool that simplifies image editing. With a few clicks, you can isolate your subject from the background, making edits quick and easy. Designed for speed, it allows you to remove backgrounds in seconds, whether you're a beginner or an expert.",
+                buttonText: "Try It Now",
+                imageSrc: "/bgremover.png",
+                imageAlt: "Image upscaler"
+              },
+              {
+                title: "Image resizer",
+                description:
+                  "Photoshop's image resizer is incredibly user-friendly. With just a few clicks, you can quickly adjust the size of your images without losing quality. It's designed for speed, allowing you to resize multiple images in no time. Perfect for anyone needing efficient image editing!",
+                buttonText: "Try It Now",
+                imageSrc:
+                  "/media_12d729f5e1b50b373d1ef77bd90c7d7925c60ca55.mp4",
+                imageAlt: "Image resizer"
+              },
+              {
+                title: "Image Upscaler",
+                description:
+                  "An AI image upscaler is a digital tool that uses artificial intelligence to enlarge low-resolution images while preserving quality. It analyzes the original image and adds pixels to enhance resolution, keeping the details, textures, and colors intact.",
+                buttonText: "Try It Now",
+                imageSrc: "/upscaler.mp4",
+                imageAlt: "Image upscaler"
+              },
+              {
+                title: "Ai Image Generator",
+                description:
+                  "An AI image generator is an advanced tool that uses machine learning to create descriptive captions for images, analyzing visual elements to provide context and meaning.",
+                buttonText: "Try It Now",
+                imageSrc: "/aiGenerated.mp4",
+                imageAlt: "aiGenerated image"
+              }
+            ].map((service, index) => (
+              <Card
+                key={index}
+                className={cn(
+                  index % 2 ? "flex-row-reverse" : "flex-row",
+                  "py-12 px-24 flex"
+                )}
+              >
+                <div className="flex-1">
+                  <h3 className="text-xl font-semibold mb-4">
+                    {service.title}
+                  </h3>
+                  <p className="text-gray-400 mb-4">{service.description}</p>
+                  <Button
+                    variant="gooeyLeft"
+                    className="bg-white hover:bg-white/80"
+                  >
+                    {service.buttonText}
+                  </Button>
+                </div>
+                <div
+                  className={cn(
+                    index % 2 ? "justify-start" : "justify-end",
+                    "flex-1 flex items-center"
+                  )}
+                >
+                  {
+                    // @ts-expect-ignore
+                    service.imageSrc.endsWith(".mp4") ? (
+                      <video
+                        src={service.imageSrc}
+                        autoPlay
+                        loop
+                        muted
+                        className="rounded-lg"
+                        width={200}
+                        height={200}
+                      />
+                    ) : (
+                      <Image
+                        src={service.imageSrc}
+                        alt={service.imageAlt}
+                        width={200}
+                        height={200}
+                        className="size-44 object-cover rounded-lg"
+                        loading="lazy"
+                        decoding="async"
+                      />
+                    )
+                  }
+                </div>
+              </Card>
+            ))}
+          </div>
+        </div>
         {/* Testimonial Section */}
-        <section className="container mx-auto px-4 py-16 text-center">
+        <section className="container px-4 py-16 text-center">
           <div className="max-w-2xl mx-auto">
             <p className="text-lg text-gray-300 mb-6">
               Using Photocess is a breeze! Its user-friendly tools make it
